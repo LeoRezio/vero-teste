2ad5f0cb4ab62389988823d63e3bb5115865d4ff
LeoRezio <leonardo.rezio@gmail.com>
2022-01-03 15:32:21 -0300

    Aprimorando Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Dashboard from './component/dashboard';
 
 function App() {
--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -1,6 +1,9 @@
 import {useState,useEffect} from "react";
 import api from '../service/api.service';
-import {Table} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Table from "react-bootstrap/Table";
+import Dropdown from "react-bootstrap/Dropdown";
+
 
 const Dashboard = () => {
     const [data, setData] = useState([]);
@@ -18,27 +21,54 @@ const Dashboard = () => {
         return() => clearInterval(load);
     }, [data]);
 
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
     return (
-        <>
-        <Table responsive>
-            <thead>
-              <tr>
-                <th>#</th>
-                  <th>Table heading</th>
-              </tr>
-            </thead>
-            <tbody>
-            {data?.map((info) => (
-              <tr>
-                <td>{info.id}</td>
-                <td>{info.origem}</td>
-                <td>{info.destino}</td>
-                <td>{info.estado}</td>
-              </tr>
-              ))}
-            </tbody>
-        </Table>
-        </>
+        <Container className="mx-auto">
+            <Table striped bordered responsive >
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Origem</th>
+                    <th>Destino</th>
+                    <th>Estado</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {data?.map((info) => (
+                  <tr key={info.id}>
+                    <td>{info.id}</td>
+                    <td>{info.origem}</td>
+                    <td>{info.destino}</td>
+                    <td>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="outline-secondary" id={`dropdown-${info.id}`}>
+                            {capitalize(info.estado)}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Em curso</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Chamando</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Em Fluxo de Seleção</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </td>
+                  </tr>
+                  ))}
+                </tbody>
+            </Table>
+            <Dropdown>
+              <Dropdown.Toggle drop="end" variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>          
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
     )
 
 }
