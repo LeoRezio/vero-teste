f161bf42573889dccf17ae18b6796b8f05b1943f
LeoRezio <leonardo.rezio@gmail.com>
2022-01-04 20:47:20 -0300

    Implementação de Modal, Retorno de Status e Alteração de Estado

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -3,54 +3,71 @@ import api from '../service/api.service';
 import Container from 'react-bootstrap/Container';
 import Table from "react-bootstrap/Table";
 import Dropdown from "react-bootstrap/Dropdown";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 
 const Dashboard = () => {
     const [data, setData] = useState([]);
-    const time = 60000;
+    const [modal,setModal] = useState(false);
+    const [status,setStatus] = useState()
+    const time = 30000;
 
     useEffect(() => {
-        const load = setInterval(() => {
-            return (
-                api.get(`http://191.252.93.122/desafio-front-end/api/index.php`).then((res) => {
-                    console.log(res.data);
-                    setData(res.data);
-                })
-            )
-        }, time);
-        return() => clearInterval(load);
+      const load = setInterval(() => {
+          return (
+              api.get(`http://191.252.93.122/desafio-front-end/api/index.php`).then((res) => {
+                  console.log(res.data);
+                  setData(res.data);
+              })   
+          )
+      }, time);
+      return() => clearInterval(load);
     }, [data]);
 
-    function capitalize(string) {
+    
+    const capitalize = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
       }
-
+    const updateState = (id, selected) => {
+      const params = { id: id, estado: selected}
+      api.get(`http://191.252.93.122/desafio-front-end/api/update.php`, {params} ).then((res) => {
+        console.log(res.data);
+        console.log(params);
+        setStatus(res.data.status);
+        if (res.data.status === 200) {
+          data[id - 1].estado = selected;
+        }
+      })
+      setModal(true);
+    }
+    const closeModal = () => setModal(false)
     return (
         <Container className="mx-auto">
             <Table striped bordered responsive >
                 <thead>
                   <tr>
-                    <th>ID</th>
-                    <th>Origem</th>
-                    <th>Destino</th>
-                    <th>Estado</th>
+                    <th className="text-center">ID</th>
+                    <th className="text-center">Origem</th>
+                    <th className="text-center">Destino</th>
+                    <th className="text-center">Estado</th>
                   </tr>
                 </thead>
                 <tbody>
                 {data?.map((info) => (
                   <tr key={info.id}>
-                    <td>{info.id}</td>
-                    <td>{info.origem}</td>
-                    <td>{info.destino}</td>
+                    <td className="text-center">{info.id}</td>
+                    <td className="text-center">{info.origem}</td>
+                    <td className="text-center">{info.destino}</td>
                     <td>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="outline-secondary" id={`dropdown-${info.id}`}>
+                        <Dropdown className="text-center" onSelect={(selectedKey) => updateState(info.id, selectedKey)}>
+                            <Dropdown.Toggle  variant="outline-secondary" id={`dropdown-${info.id}`}>
                             {capitalize(info.estado)}
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Em curso</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Chamando</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Em Fluxo de Seleção</Dropdown.Item>
+                                <Dropdown.Item eventKey="em curso">Em curso</Dropdown.Item>
+                                <Dropdown.Item eventKey="chamando">Chamando</Dropdown.Item>
+                                <Dropdown.Item eventKey="em fluxo de selecao">Em Fluxo de Seleção</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     </td>
@@ -58,16 +75,21 @@ const Dashboard = () => {
                   ))}
                 </tbody>
             </Table>
-            <Dropdown>
-              <Dropdown.Toggle drop="end" variant="success" id="dropdown-basic">
-                Dropdown Button
-              </Dropdown.Toggle>          
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Modal
+              show={modal}
+              onHide={closeModal}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Body className="fs-2 text-center">
+                {status}
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={closeModal}>
+                  Fechar
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Container>
     )
 
